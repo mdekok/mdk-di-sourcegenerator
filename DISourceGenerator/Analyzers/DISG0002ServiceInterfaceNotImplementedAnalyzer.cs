79056fe022044a11c95ef18ee6d7fb125c459fc0
Michel de Kok <info@micheldekok.nl>
2024-01-12 15:53:25 +0100

    Include analyzers in source generator

---
+++ DISourceGenerator/Analyzers/DISG0002ServiceInterfaceNotImplementedAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using Mdk.DISourceGenerator.Analyzers.Lib;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Linq;
+
+namespace Mdk.DISourceGenerator.Analyzers;
+
+/// <summary>DI002 analyzer: Interface missing on class</summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class DISG0002ServiceInterfaceNotImplementedAnalyzer : DISGAnalyzerBase
+{
+    /// <inheritdoc/>
+    protected override DiagnosticDescriptor BuildRule() => new(
+        "DI002",
+        "Interface missing on class",
+        "Class '{0}' does not implement registered interface '{1}'",
+        Constants.DiagnosticCategory,
+        DiagnosticSeverity.Error,
+        true);
+
+    /// <inheritdoc/>
+    protected override Diagnostic? AnalyzeAttribute(
+        INamedTypeSymbol classType,
+        INamedTypeSymbol? serviceType,
+        INamedTypeSymbol? implementationType)
+    {
+        // [Add{Lifetime}<Interface>]
+        // class Implementation { } // Implementation must implement Interface.
+
+        if (serviceType is null)
+            return null;
+
+        if (serviceType.TypeKind == TypeKind.Interface
+            && !serviceType.IsGenericType
+            && !classType
+                .AllInterfaces
+                .Any(interfaceType => interfaceType.Equals(serviceType, SymbolEqualityComparer.Default)))
+            return Diagnostic.Create(Rule,
+                classType.Locations[0],
+                classType.Name, serviceType.Name);
+
+        return null;
+    }
+}
---
+++ DISourceGenerator/Analyzers/Lib/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Mdk.DISourceGenerator.Analyzers.Lib;
+
+internal static class Constants
+{
+    internal const string DiagnosticCategory = "Generator";
+
+    internal const string DIAttributesNamespace = "Mdk.DIAttributes";
+}

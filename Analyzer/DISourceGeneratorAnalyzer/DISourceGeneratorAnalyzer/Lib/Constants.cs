630aab4b452be332ed92142bc30cf10d519be0f2
Michel de Kok <info@micheldekok.nl>
2024-01-12 20:21:57 +0100

    Clean up

--- Analyzer/DISourceGeneratorAnalyzer/DISourceGeneratorAnalyzer/Lib/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DISourceGeneratorAnalyzer.Lib;
-
-internal static class Constants
-{
-    internal const string DiagnosticCategory = "Generator";
-
-    internal const string DIAttributesNamespace = "Mdk.DIAttributes";
-}
